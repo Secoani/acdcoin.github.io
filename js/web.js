e74d800db95668e66085465e3df0b78ef8f115d0
ardabasa <arda@turkpin.com>
2024-10-21 12:04:49 +0300

    arrange the files

--- js/web.js
+++ js/web.js
@@ -3,7 +3,6 @@ document.addEventListener('DOMContentLoaded', () => {
     const links = document.querySelectorAll('nav ul li a');
     links.forEach(link => {
         link.addEventListener('click', () => {
-            alert(`Navigating to ${link.textContent}`);
         });
     });
 });
