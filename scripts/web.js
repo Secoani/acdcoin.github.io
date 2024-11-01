fa16e062b2f1b9202dd7a6077cbbcdab80285313
Burak <burakkonca361@gmail.com>
2024-11-02 01:19:47 +0300

    Edit WebSite
    optimized for mobile devices, except for mining, there are minor effect issues

--- scripts/web.js
+++ scripts/web.js
@@ -1,11 +1,4 @@
 // web.js
-document.addEventListener('DOMContentLoaded', () => {
-    const links = document.querySelectorAll('nav ul li a');
-    links.forEach(link => {
-        link.addEventListener('click', () => {
-        });
-    });
-});
 function changeLanguage(lang) {
     if (lang === "tr") {
         window.location.href = "index_tr.html";
@@ -15,16 +8,4 @@ function changeLanguage(lang) {
     }
 }
 
-window.addEventListener('resize', () => {
-    const navLinks = document.querySelector('.nav-links');
-    
-    // Ekran genişliği 857 pikselin üstündeyse menüyü kapat
-    if (window.innerWidth > 857 && navLinks.classList.contains('show')) {
-        navLinks.classList.remove('show');
-    }
-});
 
-function toggleMenu() {
-    const navLinks = document.querySelector('.nav-links');
-    navLinks.classList.toggle('show');
-}
