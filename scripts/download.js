552b48eceefed8cc8a71a6e65248e1cf880921e8
ardabasa <arda@turkpin.com>
2024-11-12 09:43:52 +0300

    Refactor language selection and remove unused scripts

--- scripts/download.js
+++ scripts/download.js
@@ -40,22 +40,3 @@ function setLanguage(lang) {
     langSelect.querySelector('option[value="tr"]').textContent = content[lang].langTurkish;
     langSelect.querySelector('option[value="en"]').textContent = content[lang].langEnglish;
 }
-
-// Dili değiştir ve seçili dili localStorage'da sakla
-function changeLanguage(lang) {
-    setLanguage(lang);
-    localStorage.setItem('language', lang); // Seçilen dili localStorage'a kaydet
-
-    // URL'yi güncelle
-    const url = new URL(window.location.href);
-    url.searchParams.set('lang', lang); // `lang` parametresini güncelle
-    window.history.pushState({}, '', url); // URL'yi güncelle ve sayfa yenilenmeden adres çubuğunu değiştir
-}
-
-// URL'den dil parametresini oku ve sayfa yüklendiğinde ayarla
-const urlParams = new URLSearchParams(window.location.search);
-const urlLang = urlParams.get('lang');
-if (urlLang) {
-    changeLanguage(urlLang);
-    localStorage.setItem('language', urlLang); // URL'den gelen dili localStorage'a kaydet
-}
--- scripts/web.js
+++ scripts/web.js
@@ -68,26 +68,6 @@ function setLanguage(lang) {
     // Tüm bağlantıları güncelleyerek dil parametresini ekle
     updateNavLinks(lang);
 }
-
-// Dili değiştir ve seçili dili localStorage'da sakla
-function changeLanguage(lang) {
-    setLanguage(lang);
-    localStorage.setItem('language', lang); // Seçilen dili localStorage'a kaydet
-
-    // URL'yi güncelle
-    const url = new URL(window.location.href);
-    url.searchParams.set('lang', lang); // `lang` parametresini güncelle
-    window.history.pushState({}, '', url); // URL'yi güncelle ve sayfa yenilenmeden adres çubuğunu değiştir
-}
-
-// URL'den dil parametresini oku ve sayfa yüklendiğinde ayarla
-const urlParams = new URLSearchParams(window.location.search);
-const urlLang = urlParams.get('lang');
-if (urlLang) {
-    changeLanguage(urlLang);
-    localStorage.setItem('language', urlLang); // URL'den gelen dili localStorage'a kaydet
-}
-
 window.addEventListener('resize', () => {
     const navLinks = document.querySelector('.nav-links');
     if (window.innerWidth > 857 && navLinks.classList.contains('show')) {
