b193d1f4c802e9116da777fc6034d35cdde3db20
ardabasa <arda@turkpin.com>
2024-11-14 09:54:01 +0300

    Update Turkish translations and improve language handling in team, contact, and about us pages

--- assets/js/transaction.js
+++ assets/js/transaction.js
@@ -1,32 +1,3 @@
-// transaction.js
-
-// Seçili dili ayarlayan fonksiyon
-function setLanguage(lang) {
-    // Burada dil içeriğini güncelleme kodlarını ekleyin
-    if (lang === "tr") {
-        document.querySelector("h1").innerText = "ACDcoin İşlem"; // Başlık güncellemesi
-        document.querySelector(".back-button").innerHTML = "Geri";
-        document.querySelector('label[for="sender"]').innerHTML =
-            'Gönderen Adresi <span class="tooltip">? <span class="tooltip-text">ACDcoin gönderen adresini girin.</span></span>';
-        document.querySelector('label[for="receiver"]').innerHTML =
-            'Alıcı Adresi <span class="tooltip">? <span class="tooltip-text">Alıcının adresini girin.</span></span>';
-        document.querySelector('label[for="amount"]').innerHTML =
-            'Miktar (ACDcoin) <span class="tooltip">? <span class="tooltip-text">Göndermek istediğiniz ACDcoin miktarını belirtin.</span></span>';
-        document.querySelector('button[type="submit"]').innerText =
-            "ACDcoin Gönder"; // Buton metni
-    } else {
-        document.querySelector("h1").innerText = "ACDcoin Transaction"; // Başlık güncellemesi
-        document.querySelector(".back-button").innerHTML = "Back";
-        document.querySelector('label[for="sender"]').innerHTML =
-            'Sender Address <span class="tooltip">? <span class="tooltip-text">Enter the address sending the ACDcoin.</span></span>';
-        document.querySelector('label[for="receiver"]').innerHTML =
-            'Receiver Address <span class="tooltip">? <span class="tooltip-text">Enter the recipient\'s address.</span></span>';
-        document.querySelector('label[for="amount"]').innerHTML =
-            'Amount (ACDcoin) <span class="tooltip">? <span class="tooltip-text">Specify the amount of ACDcoin to send.</span></span>';
-        document.querySelector('button[type="submit"]').innerText =
-            "Send ACDcoin"; // Buton metni
-    }
-}
 // Form doğrulama fonksiyonu
 function validateForm(event) {
     event.preventDefault(); // Formun otomatik olarak gönderilmesini engelle
